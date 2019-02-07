0a254d6a9928c1b995d78e0eadc39f8c2250be5e
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-02-07 22:17:44 +0900

    rice

---
+++ components/Motto.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-parallax src="/rice_usd_koshihikari_rice.jpg" height="600">
+    <v-layout column align-center justify-center class="white--text">
+      <h1 class="mb-2 display-3 text-xs-center">
+        おいしいお米を食べたい
+      </h1>
+    </v-layout>
+  </v-parallax>
+</template>
---
+++ components/RiceDetails.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-layout column wrap class="my-5" align-center>
+    <v-flex xs12 sm4 class="my-3">
+      <div class="text-xs-center">
+        <h2 class="headline">
+          独自製法による栽培
+        </h2>
+        <span class="subheading">
+          SZM株式会社では独自開発した製法により、安全でおいしいお米を栽培しています。
+        </span>
+      </div>
+    </v-flex>
+    <v-flex xs12>
+      <v-container grid-list-xl>
+        <v-layout row wrap align-center>
+          <v-flex xs12 md4>
+            <v-card class="elevation-0 transparent">
+              <v-card-title primary-title class="layout justify-center">
+                <div class="headline text-xs-center">
+                  種まき・間引き・定植
+                </div>
+              </v-card-title>
+              <v-card-text>
+                よく実った重い種もみを選んで、苗床に植えます。育った苗を植える田んぼを準備します。田んぼに水を入れ、トラクターで水と土を混ぜながら、田んぼを平らにします。
+                苗が育ち葉が5枚ぐらい出て、気温が13℃を超えると、田んぼの水があたたかくなってきますので、苗を田んぼに植えます。
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs12 md4>
+            <v-card class="elevation-0 transparent">
+              <v-card-title primary-title class="layout justify-center">
+                <div class="headline">
+                  管理
+                </div>
+              </v-card-title>
+              <v-card-text>
+                株が育ってくると、茎の根本から新しい茎が生えてきます。これを分けつと言いますが、稲は、この茎が20本ぐらいになるまで、分けつを繰り返します。稲が育っている間に田んぼの雑草も育っているので、雑草をこまめに抜きます。そうしないと、雑草に養分をとられてしまい、稲の育ちが悪くなります。
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs12 md4>
+            <v-card class="elevation-0 transparent">
+              <v-card-title primary-title class="layout justify-center">
+                <div class="headline text-xs-center">
+                  収穫
+                </div>
+              </v-card-title>
+              <v-card-text>
+                いなほが固くなってきたら、もう水は必要がないので、田んぼの水を抜きます。いなほが黄色くなったら収穫のサインです。刈り取った稲はしばらく乾かします。稲がかわいたら、モミの部分だけをいなほから取り外します。この作業を脱穀と言います。刈り入れたモミからモミガラをはずして中身だけを取り出します。これを玄米と言います。
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
+  </v-layout>
+</template>
